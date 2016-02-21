00f13e1535e80aa3afb319eabdd2018aaa11fb7b
Andrii Stasiuk <sir.xeed@gmail.com>
2016-02-21 18:16:26 +0200

    initial commit

--- Java Basics 03/src/Doubles.java
+++ Java Basics 03/src/Doubles.java
@@ -10,7 +10,14 @@ public class Doubles {
     }
 
     public static class ParseData {
-        private int number = 0;
+        private double number = 0;
+        private boolean decimal = false;
+        private boolean isPositive = true;
+        private int decimalPow = 1;
+
+        public void setDecimal(boolean decimal) {
+            this.decimal = decimal;
+        }
 
         public void setIsPositive(boolean isPositive) {
             this.isPositive = isPositive;
@@ -24,14 +31,18 @@ public class Doubles {
             }
         }
 
-        private boolean isPositive = true;
-
-        private int getNumber() {
+        private double getNumber() {
             return number;
         }
 
         public void addDigit(int digit) {
-            number = number * 10 + digit;
+            if (decimal) {
+                number += digit / (Math.pow(10, decimalPow));
+                decimalPow++;
+            } else {
+                number = number * 10 + digit;
+            }
+
         }
     }
 
@@ -62,6 +73,13 @@ public class Doubles {
                         parseData.setIsPositive(false);
                         return NUMBER;
                     }
+                    if (c == '+') {
+                        return NUMBER;
+                    }
+                    if (c == '.') {
+                        parseData.setDecimal(true);
+                        return NUMBER;
+                    }
                     return INVALID_END;
                 }
             }, NUMBER {
@@ -69,6 +87,10 @@ public class Doubles {
                 public State next(char c, ParseData parseData) {
                     if (c - '0' >= 0 && c - '0' <= 9) {
                         parseData.addDigit(c - '0');
+                        return NUMBER;
+                    }
+                    if (c == '.') {
+                        parseData.setDecimal(true);
                         return NUMBER;
                     }
                     return INVALID_END;
