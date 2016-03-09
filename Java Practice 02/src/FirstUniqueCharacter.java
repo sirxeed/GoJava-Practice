3175b0f896c809c08b735897694f28109965593c
Andrii Stasiuk <sir.xeed@gmail.com>
2016-03-09 22:35:42 +0200

    initial commit

--- Java Practice 02/src/FirstUniqueCharacter.java
+++ Java Practice 02/src/FirstUniqueCharacter.java
@@ -1,53 +1,21 @@
 public class FirstUniqueCharacter {
-    public char find(String s) {
+    public Character find(String s) {
+        Character target = null;
 
-    }
-
-    /*
-    public char find(String s) {
-        if (s.length() == 1) {
+        if (s.length() < 2) {
             return s.charAt(0);
         }
 
-        boolean isNotRepeated;
-        for (int i = 0; i < s.length(); i++) {
-            isNotRepeated = true;
-            for (int j = 0; j < s.length(); j++) {
-                if (i == j) {
-                    j++;
-                }
-                if (s.charAt(i) == s.charAt(j)) {
-                    isNotRepeated = false;
-                    break;
-                }
-
+        while (s != null && !s.equals("")) {
+            char ch = s.charAt(0);
+            String postSubString = s.substring(1, s.length());
+            if (!postSubString.contains(String.valueOf(ch))) {
+                target = ch;
+                break;
             }
-            if (isNotRepeated) {
-                return s.charAt(i);
-            }
+            s = s.replaceAll(String.valueOf(ch), "");
         }
-        return 0;
+        return target;
+
     }
-    */
-
-    /*
-        public Character find(String s) {
-            Character target = null;
-
-            if (s.length() < 2) {
-                return s.charAt(0);
-            }
-
-            while (s != null && !s.equals("")) {
-                char ch = s.charAt(0);
-                String postSubString = s.substring(1, s.length());
-                if (!postSubString.contains(String.valueOf(ch))) {
-                    target = ch;
-                    break;
-                }
-                s = s.replaceAll(String.valueOf(ch), "");
-            }
-            return target;
-        */
-
 }
